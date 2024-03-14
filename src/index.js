6bbf121cc8d18963beaa59ea70c84e4f81189e2e
becauseidontlike <120679852+becauseidontlike@users.noreply.github.com>
2024-03-14 21:00:54 +0100

    hover event

--- src/index.js
+++ src/index.js
@@ -9,8 +9,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
-    {/* <StarRating maxRating={15} />
-    <StarRating /> */}
+    <StarRating maxRating={10} />
   </React.StrictMode>
 );
