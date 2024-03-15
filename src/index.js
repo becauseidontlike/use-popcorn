555720fcf439d757217748e333dbfa1039650d85
becauseidontlike <120679852+becauseidontlike@users.noreply.github.com>
2024-03-15 21:23:24 +0100

    reusability of star prop

--- src/index.js
+++ src/index.js
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 
 import StarRating from "./StarRating";
 
+function Test() {
+  const [movieRating, setMovieRating] = useState();
+  return (
+    <div>
+      <StarRating color="violet" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars.</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
     <StarRating maxRating={10} />
+    <StarRating
+      maxRating={5}
+      color="pink"
+      size={30}
+      className="test"
+      messages={["terrible", "bad", "okay", "good", "top"]}
+    />
+    <StarRating
+      maxRating={5}
+      color="blue"
+      size={30}
+      className="test"
+      messages={["terrible", "bad", "okay", "good", "top"]}
+      defaultRating={3}
+    />
+    <Test />
   </React.StrictMode>
 );
