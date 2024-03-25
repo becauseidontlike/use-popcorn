a6bf0ec30f06b63dc4e1a40fda3295aac4de62ec
becauseidontlike <120679852+becauseidontlike@users.noreply.github.com>
2024-03-25 21:05:21 +0100

    cleaning up data fetching - race controller

--- src/App.js
+++ src/App.js
@@ -85,12 +85,15 @@ export default function App() {
 
   useEffect(
     function () {
+      const controller = new AbortController();
+
       async function fetchMovies() {
         try {
           setIsLoadiing(true);
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${mykey}&s=${query}`
+            `http://www.omdbapi.com/?apikey=${mykey}&s=${query}`,
+            { signal: controller.signal }
           );
 
           if (!res.ok)
@@ -101,9 +104,12 @@ export default function App() {
           if (data.Response === "False") throw new Error("Movie not found.");
 
           setMovies(data.Search);
+          setError("");
         } catch (err) {
           // console.log(err.message);
-          setError(err.message);
+          if (err.name !== "AbortError") {
+            setError(err.message);
+          }
         } finally {
           setIsLoadiing(false);
         }
@@ -114,6 +120,10 @@ export default function App() {
         return;
       }
       fetchMovies();
+
+      return function () {
+        controller.abort();
+      };
     },
     [query]
   );
