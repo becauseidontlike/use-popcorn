d313cc629300c68c98c678ca0d88186ef569b15f
becauseidontlike <120679852+becauseidontlike@users.noreply.github.com>
2024-04-04 22:30:45 +0200

    custom hook

---
+++ src/useMovies.js
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+
+export function useMovies(query) {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoadiing] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(
+    function () {
+      const controller = new AbortController();
+
+      async function fetchMovies() {
+        try {
+          setIsLoadiing(true);
+          setError("");
+          const res = await fetch(
+            `http://www.omdbapi.com/?apikey=${mykey}&s=${query}`,
+            { signal: controller.signal }
+          );
+
+          if (!res.ok)
+            throw new Error("Something went wrong with fetching movies...");
+
+          const data = await res.json();
+
+          if (data.Response === "False") throw new Error("Movie not found.");
+
+          setMovies(data.Search);
+          setError("");
+        } catch (err) {
+          // console.log(err.message);
+          if (err.name !== "AbortError") {
+            setError(err.message);
+          }
+        } finally {
+          setIsLoadiing(false);
+        }
+      }
+      if (query.length < 3) {
+        setMovies([]);
+        setError("");
+        return;
+      }
+      handleCloseMovie();
+      fetchMovies();
+
+      return function () {
+        controller.abort();
+      };
+    },
+    [query]
+  );
+}
