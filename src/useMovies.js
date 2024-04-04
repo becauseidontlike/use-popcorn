8a3b0d4690e1841c1fcd30270dd12dfd4de5c24b
becauseidontlike <120679852+becauseidontlike@users.noreply.github.com>
2024-04-04 22:43:58 +0200

    callback removed

--- src/useMovies.js
+++ src/useMovies.js
@@ -8,6 +8,8 @@ export function useMovies(query) {
 
   useEffect(
     function () {
+      // callback?.();
+
       const controller = new AbortController();
 
       async function fetchMovies() {
